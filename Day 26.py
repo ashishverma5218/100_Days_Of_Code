647ca509a2467f542f07f5310bab7743a91f6c58
Ashish Verma <100463212+ashishverma5218@users.noreply.github.com>
2024-02-13 09:56:44 +0530

    Add files via upload

---
+++ Day 26.py
@@ -0,0 +1,8 @@
+# l = input().split(',')
+# numbers = [int(x) for x in l]
+# r = [num for num in numbers if num % 2 == 0]
+# print(r)
+
+l1 = [2,3,4,5,6,7]
+l2 = [2,46,9,5,1]
+r = print([int(num) for num in l1 if num in l2])
