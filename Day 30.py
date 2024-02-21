3d80cfe57fa32328278ce0f99f4263b3e175e67b
Ashish Verma <100463212+ashishverma5218@users.noreply.github.com>
2024-02-21 20:50:34 +0530

    Add files via upload

---
+++ Day 30.py
@@ -0,0 +1,21 @@
+# try:
+#     file = open("a_file.txt")
+# except:
+#     # open("a_file.txt","w")
+# else:
+#
+# finally:
+
+
+
+# fruits = eval(input())
+# def make_pie(index):
+#     try:
+#         fruit = fruits[index]
+#     except IndexError:
+#         print("fruit pie")
+#     else:
+#         print(fruit + " pie")
+#
+# make_pie(4)
+
